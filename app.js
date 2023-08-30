85ab956249a00c3842401af7f0cbbe0ff5c15061
Mohammedaliyassen <73065131+Mohammedaliyassen@users.noreply.github.com>
2023-08-31 00:22:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function selected(par){
         document.getElementById('rate'+i).style.cssText=' background-color: #262e39; color:#fff';  
         }
     document.getElementById('butt1').addEventListener('click',()=>{
-        document.getElementById('rotateMagic').style.cssText = 'transform: translate(-26vw, 20vh);'
+        document.getElementById('rotateMagic').style.cssText = 'transform: translate(-95vw, 0px);;'
         })
         document.querySelector('#fin').innerHTML =`You selected ${par} out of 5 `
     }
